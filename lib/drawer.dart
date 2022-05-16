981a952c7e396a7377b697295e955493a85d5028
abraham odianjo <odianjoabraham@outlook.com>
2022-05-16 15:45:43 +0100

    create two new skeleton file called the addblog page and the manage blog page

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,15 +1,17 @@
 import  'package:flutter/material.dart';
 import 'home.dart';
+import '';
 
 class NavigationDrawerWidget extends StatelessWidget {
-  const NavigationDrawerWidget({Key? key}) : super(key: key);
 
+  final padding =EdgeInsets.symmetric(horizontal: 20);
   @override
   Widget build(BuildContext context){
     return Drawer(
       child: Material(
         color: Colors.red,
         child: ListView(
+          padding: padding,
         children: <Widget>[
           const SizedBox(
             height:48,
@@ -56,6 +58,11 @@ void selectedItem(BuildContext context, int index){
   switch(index){
     case 0:
       Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyHomePage(title: 'feed',
+      ),
+      ));
+      break;
+    case 1:
+      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>AddBlogPage(title: 'Add Blog',
       ),
       ));
 }
