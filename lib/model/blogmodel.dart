667b5604b87859f1f389ef75fcbb5ac6cb2e6d5e
abraham odianjo <odianjoabraham@outlook.com>
2022-05-17 23:39:18 +0100

    initialise the blog to ensure it accepts the fields

--- lib/model/blogmodel.dart
+++ lib/model/blogmodel.dart
@@ -1,5 +1,7 @@
 final String blogTable = 'blogmodel';
 
+//create the blog fields for the table
+// these include the id, number, title, descrption of the blog, then the time. while is important
 class BlogFields {
   static final List<String> values =[
     id, isImportant, number, title, description, time
@@ -11,6 +13,31 @@ class BlogFields {
   static final String title = 'title';
   static final String description = 'description';
   static final String time = 'time';
+
+}
+
+// now we create the blog model to accept the types and attributes
+class Blog{
+
+    // set the type of input
+    final int? id;
+    final bool isImportant;
+    final int number;
+    final String title;
+    final String description;
+    final DateTime createdTime;
+
+  //next is to initialise the blog
+    const Blog ({
+      this.id,
+      required this.isImportant,
+      required this.number,
+      required this.title,
+      required this.description,
+      required this.createdTime
+
+})
+
 
 
 }
