4e976632cf6bf14e2bdd663a70d47a41a783dc38
abraham odianjo <odianjoabraham@outlook.com>
2022-05-17 18:00:42 +0100

    add new field for the database by calling sqflite

--- lib/database/blogdatabase.dart
+++ lib/database/blogdatabase.dart
@@ -5,6 +5,15 @@ class BlogDatabase{
   //create a global field
   static final BlogDatabase instance = BlogDatabase._init();
 
+  //add a new field for the database
+  static Database? database;
+
+
   //create a private constructor
   BlogDatabase._init();
+
+  //open connetion to the database
+Future <Database> get database async{
+  if
+}
 }
