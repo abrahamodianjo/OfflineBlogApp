a22a9567d4f5fbb9e9a2f17f2e837a363b164e98
abraham odianjo <odianjoabraham@outlook.com>
2022-05-17 23:18:29 +0100

    next is to create the database table

---
+++ lib/model/blogmodel.dart
@@ -0,0 +1,16 @@
+final String blogTable = 'blogmodel';
+
+class BlogFields {
+  static final List<String> values =[
+    id, isImportant, number, title, description, time
+  ];
+
+  static final String id = 'id';
+  static final String isImportant = 'isImportant';
+  static final String number = 'number';
+  static final String title = 'title';
+  static final String description = 'description';
+  static final String time = 'time';
+
+
+}
