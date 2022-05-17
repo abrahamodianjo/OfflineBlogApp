d04abd0032b5393faedb70a67bde5b5192d2dcbf
abraham odianjo <odianjoabraham@outlook.com>
2022-05-17 20:11:51 +0100

    now we have call the createDB

--- lib/database/blogdatabase.dart
+++ lib/database/blogdatabase.dart
@@ -6,7 +6,7 @@ class BlogDatabase{
   static final BlogDatabase instance = BlogDatabase._init();
 
   //add a new field for the database
-  static Database? database;
+  static Database? _database;
 
 
   //create a private constructor
@@ -14,6 +14,16 @@ class BlogDatabase{
 
   //open connetion to the database
 Future <Database> get database async{
-  if
+  if (_database !=null) return _database!;
+
+    //if database does not exist, initialize it
+  _database = await _initDB('blog.db');
+  return _database!;
 }
+  Future<Database> _initDB(String filePath) async{
+    final dbPath = await getDatabasesPath();
+    final path =join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+  Future _createDB(Database db, int version) async{}
 }
